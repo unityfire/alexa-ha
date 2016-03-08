7955a79a3aa2877ca9bc4de6e7dc69a8ba8e66ac
arch1v1st <archivist@unityfire.com>
2016-03-07 23:49:28 -0500

    Removed unnecessary console logging

--- helper.js
+++ helper.js
@@ -5,21 +5,17 @@ var config = require('./config');
 function getItem(itemType, location) {
     //Handle variances of itemType for config value lookup
     if (itemType === 'lighting' || itemType === 'light') {
-        //console.log('getItem itemType modified:' + itemType);
         itemType = 'lights';
     }
 
     //Strip dangling ' room' in utterance for config value lookup
     var suffix = ' room';
     if (location.substr(-suffix.length) === suffix) {
-        //console.log('getItem location trimmed:' + location);
         location = location.replace(' room','');
     }
 
     for(var key in config.item) {
-        console.log('getItem key:' + key);
         if(config.item.hasOwnProperty(itemType)) {
-            //console.log('getItem val:' + config.item[itemType][location]);
             return config.item[itemType][location];
         }
     }
@@ -31,13 +27,11 @@ function getMetric(metricName, location) {
     //Strip dangling ' room' suffix for for config value lookup
     var suffix = ' room';
     if (location.substr(-suffix.length) === suffix) {
-        //console.log('getMetric location trimmed:' + location);
         location = location.replace(' room','');
     }
 
     for(var key in config.metric) {
         if(config.metric.hasOwnProperty(metricName)) {
-            //console.log('getMetric val:' + config.metric[metricName][location]);
             return config.metric[metricName][location];
         }
     }
@@ -68,7 +62,6 @@ function getColor(colorName) {
 function getMode(modeType,modeName) {
     for(var key in config.mode) {
         if(config.mode.hasOwnProperty(modeType)) {
-            //console.log('getMode val:' + config.mode[modeType][modeName]);
             return config.mode[modeType][modeName];
         }
     }
