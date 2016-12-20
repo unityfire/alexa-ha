deaa26ac5440aecc9c46273d7c570d1078fa8e49
phprehl <Philipp.Prehl@googlemail.com>
2016-12-20 21:01:27 +0100

    Add files via upload

--- config_default.js
+++ config_default.js
@@ -1,5 +1,9 @@
 /* Default Alexa HA configuration file.  Copy this to 'config.js' and adapt as needed! */
 var config = {};
+//change here for the required language
+
+var configuredLanguage = './language/languageGER'
+var language = require(configuredLanguage);
 
 /******************************** GENERAL CONFIGURATION *****************************************/
 // Name of the application/skill, which does not need to match the skills 'Invocation Name',
@@ -7,13 +11,12 @@ var config = {};
 config.applicationName = 'Alexa-HA';
 
 // AWS ASK applicationId, resembles 'amzn1.echo-sdk-ams.app.[your-unique-value-here]'
-config.applicationId = 'amzn1.echo-sdk-ams.app.XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+config.applicationId = 'amzn1.ask.skill.xxxxxx';
 
 // AWS ASK userID, resembles 'amzn1.echo-sdk-account.[your-unique-value-here]'
-config.userId = 'amzn1.echo-sdk-account.XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
-
+config.userId = 'amzn1.ask.account.xxxxxxxxx'
 // AWS ASK password, randomly generated password to be included in the Skill's endpoint URL (i.e. '?password=XXXXXXXXXXXX')
-config.password = 'XXXXXXXXXXXX';
+config.password = '';
 
 // Greeting, when saying 'Alexa, open...'
 config.greeting = "OpenHAB, at your service";
@@ -33,19 +36,19 @@ config.wolframAppId ='XXXXXX-XXXXXXXXXX';
 config.HA_name = 'OpenHAB';
 
 // HA server protocol (http/https)
-config.HA_protocol = 'https';
+config.HA_protocol = 'http';
 
 // HA server username
-config.HA_user = 'USERNAME';
+config.HA_user = '';
 
 // HA server password
-config.HA_password = 'PASSWORD';
+config.HA_password = '';
 
 // HA server FQDN/IP
-config.HA_host = 'HA_SERVER_IP';
+config.HA_host = '';
 
 // HA server port
-config.HA_port = 'HA_SERVER_PORT';
+config.HA_port = '';
 
 // HA server URL with credentials - i.e. 'http(s)://USERNAME:PASSWORD@HA_SERVER_IP:HA_SERVER_PORT' (constructed from above variables)
 config.HA_server = config.HA_protocol + '://' + config.HA_user + ':' + config.HA_password + '@' + config.HA_host + ':' + config.HA_port;
@@ -66,11 +69,11 @@ config.HA_item_voicecmd = 'ECHO_VoiceCMD';
 config.HA_item_answer = 'ECHO_Answer';
 
 // Array of available rooms that HA has devices in, used for validations & re-prompts
-config.HA_locations = ['all','house','living','great','kitchen','master bedroom','guest bedroom','data center','garage','office','foyer','utility','inside','indoor','outside','outdoor'];
+config.HA_locations = ['Küche','Wohnzimmer','Esszimmer','Diele'];
 
 // Item configuration - itemType / Location / itemName mappings
 config.item = {
-    'light': {
+    'licht': {
         'bedroom': 'Light_Group_Bed',
         'office': 'Light_Group_Office',
         'kitchen': 'Light_Group_Kitchen',
@@ -78,6 +81,8 @@ config.item = {
         'great': 'Light_Group_Great',
         'house': 'Light_Group_All',
         'all': 'Light_Group_All',
+        'wohnzimmer': 'LichtWohnzimmer_1',
+        'diele': 'LichtDiele',
         default: 'Light_Group_All'
     },
     'motion': {
@@ -169,7 +174,7 @@ config.mode = {
         'sleep': '1',
         'home': '2',
         'away': '3'
-    }    
+    }
 };
 
 // Metric names to Item mappings
@@ -232,7 +237,7 @@ config.metric = {
         'outside': 'Weather_Humidex',
         default: 'Weather_Humidex'
     }
-   
+
 };
 
 // Unit to metric mappings
@@ -262,133 +267,12 @@ config.color = {
 /******************* ASK Utterances ************************/
 // Configure ASK Intent utterances using Alexa-App - reference:
 //  https://www.npmjs.com/package/alexa-app#schema-and-utterances for syntax
-config.utterances = {
-    // Switch devices ON/OFF in a particular room
-    'Switch': [
-        "{to |} {turn|switch|flip} {ON|OFF|Action} {the|my |} {light|lights|motion lighting|fan|stove|music|ItemName} {in the |} {-|Location}",
-        "{to |} {turn|switch|flip} {ON|OFF|Action} {the|my |} {-|Location} {light|lights|motion lighting|fan|stove|music|ItemName}",
-        "{to |} {turn|switch|flip} {the|my |} {-|Location} {light|lights|motion lighting|fan|stove|music|ItemName} {ON|OFF|Action}",
-        "{to |} {turn|switch|flip} {the|my |} {light|lights|motion lighting|fan|stove|music|ItemName} {ON|OFF|Action} {in the |} {-|Location}"
-    ],
-    // Set HSB color values for lights in a particular room
-    'SetColor': [
-        "{to |} {set|change|switch} {the|my |} {-|Location} {light|lights|lighting} color {to |} {-|Color}",
-        "{to |} {set|change|switch} {the|my |} {light|lights|lighting} color in the {-|Location} {to |} {-|Color}",
-        "color {the|my |} {light|lights|lighting} in the {-|Location} {to |} {-|Color}",
-        "color {the|my |} {-|Location} {light|lights} {to |} {-|Color}",
-        "make {the|my |} {-|Location} {light|lights} {-|Color}"
-    ],
-    // Dim lights in a particular room
-    'SetLevel': [
-        "{to |} {dim|turn down|turn up|soften|adjust} {the|my |} {light|lights|lighting|ItemName} {in the |} {-|Location} to {0-100 by 5|Percent} {percent |}",
-        "{to |} {dim|turn down|turn up|soften|adjust} {the|my |} {-|Location} {light|lights|lighting|ItemName} to {0-100 by 5|Percent} {percent |}",
-        "{to |} {set|turn down|turn up|adjust} {the|my |} {-|Location} {volume|ItemName} to {0-100 by 5|Percent} {percent |}"
-    ],
-    // Set target temperature for house HVAC
-    'SetTemp': [
-        "{to |} {set|change|adjust} {the|my |} {-|Location} {thermostat|temperature} to {60-80|Degree} {degrees |}",
-        "{to |} {set|change|adjust} {the|my |} {thermostat|temperature} {in the|in my |} {-|Location} to {60-80|Degree} {degrees |}"
-    ],
-    // Set house/lighting/security/etc scenes
-    'SetMode': [
-        "{to |} {set|change|switch} {the|my |} {house|ModeType} mode to {off|work|dinner|party|bed|away|panic|relax|gaming|theatre|shower|wake up|TV|ModeName}",
-        "{to |} {set|change|switch} {the|my |} {lighting|ModeType} mode to {all off|all on|focus|energize|relax|party|night light|bed time|love shack|lava|ModeName}",
-        "{to |} {set|change|switch} {the|my |} {security|ModeType} mode to {off|sleep|home|away|ModeName}",
-        "{to |} {set|change|switch} {the|my |} {house|ModeType} to {off|work|dinner|party|bed|away|panic|relax|gaming|theatre|shower|wake up|TV|ModeName} mode",
-        "{to |} {set|change|switch} {the|my |} {lighting|ModeType} to {all off|all on|focus|energize|relax|party|night light|bed time|love shack|lava|ModeName} mode",
-        "{to |} {set|change|switch} {the|my |} {security|ModeType} to {off|sleep|home|away|ModeName} mode"
-    ],
-    // Get current item state values
-    'GetState': [
-        "{to |} {get|check} {the|my |} {-|Location} {temperature|humidity|luminance|power consumption|visibility|pressure|wind|humidex|MetricName}",
-        "{to |} {get|check} {the|my |} {temperature|humidity|luminance|power consumption|visibility|pressure|wind|humidex|MetricName} {in the |} {-|Location}",
-        "whats {the|my |} {-|Location} {temperature|humidity|luminance|power consumption|visibility|pressure|wind|humidex|MetricName}",
-        "whats {the|my |} {temperature|humidity|luminance|power consumption|visibility|pressure|wind|humidex|MetricName} {in the |} {-|Location}"
-
-    ],
-    // Get current house/lighting/security/etc scene
-    'GetMode': [
-        "{to |} {get|check} {the|my |} {house|lighting|security|ModeType} mode {set to |}",
-        "whats {the|my |} {house|lighting|security|ModeType} mode {set to |}"
-    ],
-    // Execute 'raw' voice commands, request/response handled entirely by custom HA server rules
-    'VoiceCMD': [
-        "{lets party|Input}",
-        "{call my bank|call my phone|call work|Input}",
-        "{status update|Input}",
-        "{for a |}{status update|Input}",
-        "{whats the |}{weather like|Input}",
-        "{say a |} {quote|Input}",
-        "{its} {cold in here|Input}",
-        "{its} {hot in here|Input}"
-    ],
-    // Research something arbitrary via Wolfram API call
-    'Research': [
-        "{to |} research {what is the atomic weight of boron|Question}",
-        "{to |} research {what is thirty divided by five|Question}",
-        "{to |} research {what did Alexander Graham Bell invent|Question}",
-        "{to |} research {what does eleven plus seven equal|Question}",
-        "{to |} research {when is sunset tonight|Question}",
-        "{to |} research {how tall is mount everest|Question}",
-        "{to |} research {how many miles in an acre|Question}",
-        "{to |} research {how do you take a derivative|Question}",
-        "{to |} research {where did George Washington die|Question}",
-        "{to |} research {where are the pyramids|Question}",
-        "{to |} research {Why is the sky blue|Question}"
-    ],
-    // Stop Intent
-    'Stop': [
-        "{to |} stop {that |}",
-        "{to |} quit {that |}",
-        "{to |} exit {that |}",
-        "{to |} never mind {that |}"
-    ],
-    // Cancel Intent
-    'Cancel': [
-        "{to |} cancel {that |}"
-    ],
-    // Help Intent
-    'Help': [
-        "{for|to |} help {me |}"
-    ]
-
-    //TODO implement the yes/no/repeat intents
-    //// Yes Intent
-    //'Yes': [
-    //    "yes {please |}",
-    //    "sure",
-    //    "do it",
-    //    "affirmative"
-    //],
-    //// No Intent
-    //'No': [
-    //    "no {thanks |}",
-    //    "nope",
-    //    "negative",
-    //    "negatory"
-    //],
-    //// Repeat Intent
-    //'Repeat': [
-    //    "{please |} repeat {that |}",
-    //    "say that again",
-    //    "what did you say"
-    //]
-};
+config.utterances = language.utterances
 
 /******************* ASK Responses *************************/
 // Help response & card
-config.help = {
-    'say':  [
-            'You can ask or tell me to do all sorts of things.  For example, try saying turn all lights on, dim living lights to 50 percent, ' +
-            'change office color to blue, set house thermostat to 72 degrees, or research why is the sky blue.  ' +
-            'I have sent a cheat sheet of commands to your Alexa App for further reference.'
-            ],
-    'card': [
-            'Cheat sheet: Switch on all lights / Dim living room lights to 50%  / Set office color to Blue' +
-            ' / Set house thermostat to 72 degrees / Set house mode to relax / What is the kitchen humidity / What is the house power consumption / ' +
-            'Research why is the sky blue'
-            ]
-};
-
+config.help = language.help
+config.language = language
 // Exports
 module.exports = config;
+// module.exports = language;
---
+++ language/languageGER.js
@@ -0,0 +1,155 @@
+/******************* ASK Utterances ************************/
+// Configure ASK Intent utterances using Alexa-App - reference:
+//  https://www.npmjs.com/package/alexa-app#schema-and-utterances for syntax
+var language = {};
+
+language.utterances = {
+    // Switch devices ON/OFF in a particular room
+    'Switch': [
+        "{schalte|mache|mach} {den|die|das|mein|meinen|meins|meine} {-|ItemName} {in der|im|an der|am} {-|Location} {-|Action}",
+    ],
+    // Set HSB color values for lights in a particular room
+    'SetColor': [
+        "{to |} {set|change|switch} {the|my |} {-|Location} {light|lights|lighting} color {to |} {-|Color}",
+        "{to |} {set|change|switch} {the|my |} {light|lights|lighting} color in the {-|Location} {to |} {-|Color}",
+        "color {the|my |} {light|lights|lighting} in the {-|Location} {to |} {-|Color}",
+        "color {the|my |} {-|Location} {light|lights} {to |} {-|Color}",
+        "make {the|my |} {-|Location} {light|lights} {-|Color}"
+    ],
+    // Dim lights in a particular room
+    'SetLevel': [
+        "{to |} {dim|turn down|turn up|soften|adjust} {the|my |} {light|lights|lighting|ItemName} {in the |} {-|Location} to {0-100 by 5|Percent} {percent |}",
+        "{to |} {dim|turn down|turn up|soften|adjust} {the|my |} {-|Location} {light|lights|lighting|ItemName} to {0-100 by 5|Percent} {percent |}",
+        "{to |} {set|turn down|turn up|adjust} {the|my |} {-|Location} {volume|ItemName} to {0-100 by 5|Percent} {percent |}"
+    ],
+    // Set target temperature for house HVAC
+    'SetTemp': [
+        "{to |} {set|change|adjust} {the|my |} {-|Location} {thermostat|temperature} to {60-80|Degree} {degrees |}",
+        "{to |} {set|change|adjust} {the|my |} {thermostat|temperature} {in the|in my |} {-|Location} to {60-80|Degree} {degrees |}"
+    ],
+    // Set house/lighting/security/etc scenes
+    'SetMode': [
+        "{to |} {set|change|switch} {the|my |} {house|ModeType} mode to {off|work|dinner|party|bed|away|panic|relax|gaming|theatre|shower|wake up|TV|ModeName}",
+        "{to |} {set|change|switch} {the|my |} {lighting|ModeType} mode to {all off|all on|focus|energize|relax|party|night light|bed time|love shack|lava|ModeName}",
+        "{to |} {set|change|switch} {the|my |} {security|ModeType} mode to {off|sleep|home|away|ModeName}",
+        "{to |} {set|change|switch} {the|my |} {house|ModeType} to {off|work|dinner|party|bed|away|panic|relax|gaming|theatre|shower|wake up|TV|ModeName} mode",
+        "{to |} {set|change|switch} {the|my |} {lighting|ModeType} to {all off|all on|focus|energize|relax|party|night light|bed time|love shack|lava|ModeName} mode",
+        "{to |} {set|change|switch} {the|my |} {security|ModeType} to {off|sleep|home|away|ModeName} mode"
+    ],
+    // Get current item state values
+    'GetState': [
+        "{to |} {get|check} {the|my |} {-|Location} {temperature|humidity|luminance|power consumption|visibility|pressure|wind|humidex|MetricName}",
+        "{to |} {get|check} {the|my |} {temperature|humidity|luminance|power consumption|visibility|pressure|wind|humidex|MetricName} {in the |} {-|Location}",
+        "whats {the|my |} {-|Location} {temperature|humidity|luminance|power consumption|visibility|pressure|wind|humidex|MetricName}",
+        "whats {the|my |} {temperature|humidity|luminance|power consumption|visibility|pressure|wind|humidex|MetricName} {in the |} {-|Location}"
+
+    ],
+    // Get current house/lighting/security/etc scene
+    'GetMode': [
+        "{to |} {get|check} {the|my |} {house|lighting|security|ModeType} mode {set to |}",
+        "whats {the|my |} {house|lighting|security|ModeType} mode {set to |}"
+    ],
+    // Execute 'raw' voice commands, request/response handled entirely by custom HA server rules
+    'VoiceCMD': [
+        "{lets party|Input}",
+        "{call my bank|call my phone|call work|Input}",
+        "{status update|Input}",
+        "{for a |}{status update|Input}",
+        "{whats the |}{weather like|Input}",
+        "{say a |} {quote|Input}",
+        "{its} {cold in here|Input}",
+        "{its} {hot in here|Input}"
+    ],
+    // Research something arbitrary via Wolfram API call
+    'Research': [
+        "{to |} research {what is the atomic weight of boron|Question}",
+        "{to |} research {what is thirty divided by five|Question}",
+        "{to |} research {what did Alexander Graham Bell invent|Question}",
+        "{to |} research {what does eleven plus seven equal|Question}",
+        "{to |} research {when is sunset tonight|Question}",
+        "{to |} research {how tall is mount everest|Question}",
+        "{to |} research {how many miles in an acre|Question}",
+        "{to |} research {how do you take a derivative|Question}",
+        "{to |} research {where did George Washington die|Question}",
+        "{to |} research {where are the pyramids|Question}",
+        "{to |} research {Why is the sky blue|Question}"
+    ],
+    // Stop Intent
+    'Stop': [
+        "{to |} stop {that |}",
+        "{to |} quit {that |}",
+        "{to |} exit {that |}",
+        "{to |} never mind {that |}"
+    ],
+    // Cancel Intent
+    'Cancel': [
+        "{to |} cancel {that |}"
+    ],
+    // Help Intent
+    'Help': [
+        "{for|to |} help {me |}"
+    ]
+
+    //TODO implement the yes/no/repeat intents
+    //// Yes Intent
+    //'Yes': [
+    //    "yes {please |}",
+    //    "sure",
+    //    "do it",
+    //    "affirmative"
+    //],
+    //// No Intent
+    //'No': [
+    //    "no {thanks |}",
+    //    "nope",
+    //    "negative",
+    //    "negatory"
+    //],
+    //// Repeat Intent
+    //'Repeat': [
+    //    "{please |} repeat {that |}",
+    //    "say that again",
+    //    "what did you say"
+    //]
+};
+
+/******************* ASK Responses *************************/
+// Help response & card
+language.help = {
+    'say':  [
+            'Hallo Meister Philipp und Meisterin Isa, Ihr könnt mich alles Fragen.' ],
+    'card': [
+            'Cheat sheet: Switch on all lights / Dim living room lights to 50%  / Set office color to Blue' +
+            ' / Set house thermostat to 72 degrees / Set house mode to relax / What is the kitchen humidity / What is the house power consumption / ' +
+            'Research why is the sky blue'
+            ]
+};
+  language.bye = 'tschüss!';
+
+  language.launch = "Wie kann ich helfen";
+  language.noIntent = "Ich bin unsicher was du meinst. Bitte versuche es noch einmal!";
+
+  //Switches
+  language.noSwitch = 'Das kann ich nicht schalten!';
+  language.switchAlreadyInState = function switchAlreadyInState(location,itemName,action){
+    return 'Dein '+ itemName  + ' ist bereits ' +action;
+  }
+  language.switchToState = function switchToState(location, itemName, action) {
+    return 'Schalte ' + itemName + ' ' + action ;
+  }
+  language.switchUndefinedItem = function switchUndefinedItem(location,itemName,action) {
+    return 'Ich kann ' + itemName + 'nicht ' + action + ' schalten'
+  }
+  language.switchError = function switchError(location,itemName) {
+    console.log('schaltfehler' + location + ' ' + itemName);
+    return 'Ich kann ' + itemName + ' nicht schalten';
+  }
+  language.translateAction = function translateAction(action){
+    if (action === 'EIN' || action === 'AN'){
+      return 'ON'
+    } else {
+      return 'OFF'
+    }
+  }
+
+module.exports = language;
